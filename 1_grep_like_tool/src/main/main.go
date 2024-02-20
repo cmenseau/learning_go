da796b392321f62004f1f3f2aaf3a73fb5bb5db5
c menseau <cycy.menseau@gmail.com>
2024-02-21 12:51:41 +1300

    update main to use new methods, delte test file (test moved in other files)

--- 1_grep_like_tool/src/main/main.go
+++ 1_grep_like_tool/src/main/main.go
@@ -4,71 +4,19 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"slices"
-	"strings"
-
-	"github.com/fatih/color"
 )
 
 func main() {
 
-	// parse args
+	pattern, filenames, search := parse_args(os.Args[1:])
 
-	if len(os.Args[1:]) < 2 {
-		fmt.Errorf("Expecting more than 2 args, got %v", os.Args[1:])
-	}
-
-	var options []string
-	var pattern_idx int
-
-	var args = []string(os.Args[1:])
-
-	for i, val := range args {
-		if strings.HasPrefix(val, "-") {
-			options = append(options, strings.Replace(val, "-", "", -1))
-		} else {
-			pattern_idx = i
-			break
-		}
-	}
-
-	pattern := args[pattern_idx]
-
-	filenames := args[pattern_idx+1:]
-
-	var case_insensitive bool
-	var match_whole_line bool
-	var invert_matching bool
-
-	switch {
-	case slices.Contains(options, "i"):
-		case_insensitive = true
-	case slices.Contains(options, "x"):
-		match_whole_line = true
-	case slices.Contains(options, "v"):
-		invert_matching = true
-	}
-
-	// TODO refactor as a sort of pipeline to be able to use options together
-
-	if invert_matching {
-		fmt.Print(go_through_files(pattern, filenames, select_reverse))
-	} else if match_whole_line {
-		fmt.Println(go_through_files(pattern, filenames, select_whole_line))
-	} else {
-		if case_insensitive {
-			fmt.Print(go_through_files(pattern, filenames, select_highlight_cis))
-		} else {
-			fmt.Print(go_through_files(pattern, filenames, select_highlight))
-		}
-	}
-
+	fmt.Print(go_through_files(pattern, filenames, search))
 }
 
 func go_through_files(
 	keyword string,
 	files []string,
-	select_func func(string, string) string) string {
+	search search_info) string {
 
 	var output string
 	var print_filename bool
@@ -90,7 +38,8 @@ func go_through_files(
 
 			for scanner.Scan() {
 				line := scanner.Text()
-				line_output := select_func(keyword, line)
+
+				line_output := get_output_line(keyword, line, search)
 
 				if len(line_output) != 0 {
 					if print_filename {
@@ -103,66 +52,4 @@ func go_through_files(
 		file.Close()
 	}
 	return output
-}
-
-/////// SELECT PATTERNS ON LINE FUNCTIONS //////////
-
-// Grep default behavior : select lines containing keyword, and highlight keyword
-func select_highlight(keyword string, line string) (res string) {
-	if strings.Contains(line, keyword) {
-		split := strings.Split(line, keyword)
-		res = strings.Join(split, color_red(keyword)) + "\n"
-	}
-	return
-}
-
-// case-insensitive version
-func select_highlight_cis(keyword string, line string) (res string) {
-	var lower_keyword = strings.ToLower(keyword)
-	var len_keyword = len(keyword)
-
-	if strings.Contains(strings.ToLower(line), lower_keyword) {
-		var idx int = strings.Index(strings.ToLower(line), lower_keyword)
-		if idx != -1 {
-			for idx != -1 {
-				res += line[:idx] + color_red(line[idx:idx+len_keyword])
-				line = line[idx+len_keyword:]
-
-				idx = strings.Index(strings.ToLower(line), lower_keyword)
-			}
-			res += line + "\n"
-		}
-	}
-	return
-}
-
-func select_reverse(keyword string, line string) (res string) {
-	if !strings.Contains(line, keyword) {
-		res = line + "\n"
-	}
-	return
-}
-
-func select_whole_line(keyword string, line string) (res string) {
-	if line == keyword {
-		res = line + "\n"
-	}
-	return
-}
-
-/////// COLOR OUTPUT FUNCTIONS ///////////
-
-func color_red(str string) string {
-	red := color.New(color.FgRed).SprintFunc()
-	return red(str)
-}
-
-func color_magenta(str string) string {
-	magenta := color.New(color.FgMagenta).SprintFunc()
-	return magenta(str)
-}
-
-func color_cyan(str string) string {
-	cyan := color.New(color.FgCyan).SprintFunc()
-	return cyan(str)
 }
