d66aa2718ced9015501108e78f07fd867b5d2856
c menseau <cycy.menseau@gmail.com>
2024-04-04 12:39:45 +1300

    grep actually outputs matching pattern in bold characters

--- 1_grep_like_tool/internal/colors/colors.go
+++ 1_grep_like_tool/internal/colors/colors.go
@@ -5,16 +5,16 @@ import "github.com/fatih/color"
 /////// COLOR OUTPUT FUNCTIONS ///////////
 
 func Color_red(str string) string {
-	red := color.New(color.FgRed).SprintFunc()
+	red := color.New(color.FgRed, color.Bold).SprintFunc()
 	return red(str)
 }
 
 func Color_magenta(str string) string {
-	magenta := color.New(color.FgMagenta).SprintFunc()
+	magenta := color.New(color.FgMagenta, color.Bold).SprintFunc()
 	return magenta(str)
 }
 
 func Color_cyan(str string) string {
-	cyan := color.New(color.FgCyan).SprintFunc()
+	cyan := color.New(color.FgCyan, color.Bold).SprintFunc()
 	return cyan(str)
 }
