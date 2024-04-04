c0b4792045844a95441563a54db6b8849f912b9b
c menseau <cycy.menseau@gmail.com>
2024-04-04 16:40:45 +1300

    minor

--- 1_grep_like_tool/main.go
+++ 1_grep_like_tool/main.go
@@ -12,7 +12,7 @@ func main() {
 	req, err := grep_parser.ParseArgs(os.Args[1:])
 
 	if err != nil {
-		fmt.Fprintf(os.Stderr, err.Error())
+		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(2)
 	}
 
