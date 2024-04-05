a95286d4066aa9a211d8df0e489077552c1fe72d
c menseau <cycy.menseau@gmail.com>
2024-04-05 14:10:09 +1300

    move run method into runner package

---
+++ 1_grep_like_tool/internal/runner/runner.go
@@ -0,0 +1,35 @@
+package grep_runner
+
+import (
+	"fmt"
+	grep_engine "main/internal/engine"
+	grep_file_scanner "main/internal/file_scanner"
+	grep_parser "main/internal/parser"
+	"os"
+)
+
+func Run(args []string, out *os.File) {
+	req, err := grep_parser.ParseArgs(args)
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(2)
+	}
+
+	eng := grep_engine.Engine{Request: &req}
+
+	scanner := grep_file_scanner.FileScanner{
+		Finder:    eng,
+		Paths:     req.Paths,
+		Recursive: req.Recursive,
+	}
+
+	// TODO : GoThroughFiles : give out to write line 1 by 1
+	output, err := scanner.GoThroughFiles()
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	} else {
+		fmt.Fprint(out, output)
+	}
+}
--- 1_grep_like_tool/main.go
+++ 1_grep_like_tool/main.go
@@ -1,32 +1,10 @@
 package main
 
 import (
-	"fmt"
-	grep_engine "main/internal/engine"
-	grep_file_scanner "main/internal/file_scanner"
-	grep_parser "main/internal/parser"
+	grep_runner "main/internal/runner"
 	"os"
 )
 
-func run(args []string) {
-	req, err := grep_parser.ParseArgs(args)
-
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
-		os.Exit(2)
-	}
-
-	eng := grep_engine.Engine{Request: &req}
-
-	scanner := grep_file_scanner.FileScanner{
-		Finder:    eng,
-		Paths:     req.Paths,
-		Recursive: req.Recursive,
-	}
-
-	fmt.Print(scanner.GoThroughFiles())
-}
-
 func main() {
-	run(os.Args[1:])
+	grep_runner.Run(os.Args[1:], os.Stdout)
 }
