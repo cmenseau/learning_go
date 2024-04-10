49caae457d861784a58b40e29036a58171a10cfa
c menseau <cycy.menseau@gmail.com>
2024-04-11 11:28:03 +1200

    concurrent v2 (tests OK : order of the files is handled)

--- 1_grep_like_tool/internal/file_scanner/file_scanner.go
+++ 1_grep_like_tool/internal/file_scanner/file_scanner.go
@@ -10,6 +10,10 @@ import (
 	"unicode/utf8"
 )
 
+const workersNb int = 8
+const parallelFilesCollectNb = 10
+const parallelLinesCollectNb = 20
+
 type Finder interface {
 	OutputOnLine(line string, filename string) string
 	OutputOnWholeFile(filename string) string
@@ -23,22 +27,22 @@ type FileScanner struct {
 
 func (fileScanner FileScanner) GoThroughFiles() (string, error) {
 
-	var outputCh = make(chan string, 10)
-	var workersCh = make(chan bool, 5)
+	var workersCh = make(chan bool, workersNb)
 	var resultCh = make(chan string)
 	var workersWg = sync.WaitGroup{}
+	var filesOut chan chan string = make(chan chan string, parallelFilesCollectNb)
+	// fileOut can't be a list of chan string because we fill the list step by step and reassign it
 
-	go fileScanner.collectOutput(outputCh, resultCh)
+	go fileScanner.collectOutput(filesOut, resultCh)
 
-	// TODO :
 	// order of the results is important : for each file AND for each line
+	workersWg.Add(1)
 
 	if !fileScanner.Recursive {
 		for _, filename := range fileScanner.Paths {
-			workersCh <- true
-			workersWg.Add(1)
-			go fileScanner.processFile(filename, outputCh, workersCh, &workersWg)
+			fileScanner.processFile(filename, workersCh, filesOut, &workersWg)
 		}
+
 	} else {
 		for _, filename := range fileScanner.Paths {
 
@@ -47,9 +51,7 @@ func (fileScanner FileScanner) GoThroughFiles() (string, error) {
 					return err
 				}
 				if !d.IsDir() {
-					workersCh <- true
-					workersWg.Add(1)
-					go fileScanner.processFile(path, outputCh, workersCh, &workersWg)
+					fileScanner.processFile(path, workersCh, filesOut, &workersWg)
 				}
 				return nil
 			}
@@ -62,29 +64,44 @@ func (fileScanner FileScanner) GoThroughFiles() (string, error) {
 			}
 		}
 	}
-
-	// handle output now
-
+	workersWg.Done()
 	workersWg.Wait()
-	close(outputCh) // triggers return of collector (send to result)
-
+	close(filesOut) // triggers return of collector (send to result)
 	return <-resultCh, nil
 }
 
-func (fs FileScanner) collectOutput(matchingLines <-chan string, result chan<- string) {
+func (fs FileScanner) collectOutput(filesOut chan chan string, result chan<- string) {
 	var output string
-	for line := range matchingLines {
-		output += line
+	for fileCh := range filesOut {
+		// cannot put this in a goroutine because we need to keep order here
+		for line := range fileCh {
+			output += line
+		}
 	}
 	result <- output
 }
 
-func (fs FileScanner) processFile(filename string, output chan<- string, workersCh <-chan bool, workersWg *sync.WaitGroup) {
+func (fs FileScanner) processFile(filename string, workersCh chan bool, filesOut chan chan string, workersWg *sync.WaitGroup) {
+	currentFileOut := make(chan string, parallelLinesCollectNb)
+	filesOut <- currentFileOut
+	workersCh <- true
+	workersWg.Add(1)
+	go fs.processFileConc(filename, currentFileOut, workersCh, workersWg)
+}
+
+func (fs FileScanner) processFileConc(filename string, currentFileOut chan<- string, workersCh <-chan bool, workersWg *sync.WaitGroup) {
+	defer func() {
+		close(currentFileOut)
+		<-workersCh // free a worker spot
+		workersWg.Done()
+	}()
+
 	file, err := os.Open(filename)
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Cannot open file %s because of %s", filename, err)
 	} else {
+		defer file.Close()
 
 		scanner := bufio.NewScanner(file)
 
@@ -92,16 +109,13 @@ func (fs FileScanner) processFile(filename string, output chan<- string, workers
 			line := scanner.Text()
 
 			if utf8.ValidString(line) {
-				output <- fs.Finder.OutputOnLine(line, filename)
+				currentFileOut <- fs.Finder.OutputOnLine(line, filename)
 			} // else {
 			// 	fmt.Fprintln(os.Stderr, "invalid line, not utf-8")
 			// }
 
 			// TODO : decide what to do with this
 		}
+		currentFileOut <- fs.Finder.OutputOnWholeFile(filename)
 	}
-	file.Close()
-	output <- fs.Finder.OutputOnWholeFile(filename)
-	<-workersCh
-	workersWg.Done()
 }
