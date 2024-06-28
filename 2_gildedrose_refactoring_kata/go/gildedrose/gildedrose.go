c6d8e8b8cbbf90d83ebc5cb3c88144198148eaf0
cmenseau <cycy.menseau@gmail.com>
2024-06-28 15:30:17 +1200

    add const vars

--- 2_gildedrose_refactoring_kata/go/gildedrose/gildedrose.go
+++ 2_gildedrose_refactoring_kata/go/gildedrose/gildedrose.go
@@ -11,6 +11,11 @@ func (i Item) String() string {
 	return fmt.Sprint(i.Name, " ", i.SellIn, i.Quality)
 }
 
+const AGED_BRIE string = "Aged Brie"
+const BACKSTAGE_PASS string = "Backstage passes to a TAFKAL80ETC concert"
+const SULFURAS string = "Sulfuras, Hand of Ragnaros"
+const CONJURED string = "Conjured Mana Cake"
+
 func UpdateQuality(items []*Item) {
 	for i := 0; i < len(items); i++ {
 		UpdateItem(items[i])
@@ -18,16 +23,16 @@ func UpdateQuality(items []*Item) {
 }
 
 func UpdateItem(item *Item) {
-	if item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert" {
+	if item.Name != AGED_BRIE && item.Name != BACKSTAGE_PASS {
 		if item.Quality > 0 {
-			if item.Name != "Sulfuras, Hand of Ragnaros" {
+			if item.Name != SULFURAS {
 				item.Quality = item.Quality - 1
 			}
 		}
 	} else {
 		if item.Quality < 50 {
 			item.Quality = item.Quality + 1
-			if item.Name == "Backstage passes to a TAFKAL80ETC concert" {
+			if item.Name == BACKSTAGE_PASS {
 				if item.SellIn < 11 {
 					if item.Quality < 50 {
 						item.Quality = item.Quality + 1
@@ -42,15 +47,15 @@ func UpdateItem(item *Item) {
 		}
 	}
 
-	if item.Name != "Sulfuras, Hand of Ragnaros" {
+	if item.Name != SULFURAS {
 		item.SellIn = item.SellIn - 1
 	}
 
 	if item.SellIn < 0 {
-		if item.Name != "Aged Brie" {
-			if item.Name != "Backstage passes to a TAFKAL80ETC concert" {
+		if item.Name != AGED_BRIE {
+			if item.Name != BACKSTAGE_PASS {
 				if item.Quality > 0 {
-					if item.Name != "Sulfuras, Hand of Ragnaros" {
+					if item.Name != SULFURAS {
 						item.Quality = item.Quality - 1
 					}
 				}
