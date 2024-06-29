309ac3806a25f545bef6de5e119fd2ccd891603a
c menseau <cycy.menseau@gmail.com>
2024-06-29 16:09:59 +1200

    gather all if under same else because all if have condition game.P1point != game.P2point

--- 3_tennis_refactoring_kata/go/TennisGame2.go
+++ 3_tennis_refactoring_kata/go/TennisGame2.go
@@ -33,84 +33,86 @@ func (game *tennisGame2) GetScore() string {
 		} else {
 			score = "Deuce"
 		}
+	} else {
+
+		var P1res string
+		var P2res string
+
+		if game.P1point > 0 && game.P2point == 0 {
+			if game.P1point == 1 {
+				P1res = "Fifteen"
+			}
+			if game.P1point == 2 {
+				P1res = "Thirty"
+			}
+			if game.P1point == 3 {
+				P1res = "Forty"
+			}
+
+			P2res = "Love"
+		}
+		if game.P2point > 0 && game.P1point == 0 {
+			if game.P2point == 1 {
+				P2res = "Fifteen"
+			}
+			if game.P2point == 2 {
+				P2res = "Thirty"
+			}
+			if game.P2point == 3 {
+				P2res = "Forty"
+			}
+
+			P1res = "Love"
+		}
+
+		if game.P1point > game.P2point && game.P1point < 4 {
+			if game.P1point == 2 {
+				P1res = "Thirty"
+			}
+			if game.P1point == 3 {
+				P1res = "Forty"
+			}
+			if game.P2point == 1 {
+				P2res = "Fifteen"
+			}
+			if game.P2point == 2 {
+				P2res = "Thirty"
+			}
+		}
+		if game.P2point > game.P1point && game.P2point < 4 {
+			if game.P2point == 2 {
+				P2res = "Thirty"
+			}
+			if game.P2point == 3 {
+				P2res = "Forty"
+			}
+			if game.P1point == 1 {
+				P1res = "Fifteen"
+			}
+			if game.P1point == 2 {
+				P1res = "Thirty"
+			}
+		}
+		if P1res != "" && P2res != "" {
+			score = P1res + "-" + P2res
+		}
+
+		if game.P1point > game.P2point && game.P2point >= 3 {
+			score = "Advantage player1"
+		}
+
+		if game.P2point > game.P1point && game.P1point >= 3 {
+			score = "Advantage player2"
+		}
+
+		if game.P1point >= 4 && game.P2point >= 0 && (game.P1point-game.P2point) >= 2 {
+			score = "Win for player1"
+		}
+		if game.P2point >= 4 && game.P1point >= 0 && (game.P2point-game.P1point) >= 2 {
+			score = "Win for player2"
+		}
 	}
 
-	var P1res string
-	var P2res string
-
-	if game.P1point > 0 && game.P2point == 0 {
-		if game.P1point == 1 {
-			P1res = "Fifteen"
-		}
-		if game.P1point == 2 {
-			P1res = "Thirty"
-		}
-		if game.P1point == 3 {
-			P1res = "Forty"
-		}
-
-		P2res = "Love"
-	}
-	if game.P2point > 0 && game.P1point == 0 {
-		if game.P2point == 1 {
-			P2res = "Fifteen"
-		}
-		if game.P2point == 2 {
-			P2res = "Thirty"
-		}
-		if game.P2point == 3 {
-			P2res = "Forty"
-		}
-
-		P1res = "Love"
-	}
-
-	if game.P1point > game.P2point && game.P1point < 4 {
-		if game.P1point == 2 {
-			P1res = "Thirty"
-		}
-		if game.P1point == 3 {
-			P1res = "Forty"
-		}
-		if game.P2point == 1 {
-			P2res = "Fifteen"
-		}
-		if game.P2point == 2 {
-			P2res = "Thirty"
-		}
-	}
-	if game.P2point > game.P1point && game.P2point < 4 {
-		if game.P2point == 2 {
-			P2res = "Thirty"
-		}
-		if game.P2point == 3 {
-			P2res = "Forty"
-		}
-		if game.P1point == 1 {
-			P1res = "Fifteen"
-		}
-		if game.P1point == 2 {
-			P1res = "Thirty"
-		}
-	}
-	if P1res != "" && P2res != "" {
-		score = P1res + "-" + P2res
-	}
-
-	if game.P1point > game.P2point && game.P2point >= 3 {
-		score = "Advantage player1"
-	}
-
-	if game.P2point > game.P1point && game.P1point >= 3 {
-		score = "Advantage player2"
-	}
-
-	if game.P1point >= 4 && game.P2point >= 0 && (game.P1point-game.P2point) >= 2 {
-		score = "Win for player1"
-	}
-	if game.P2point >= 4 && game.P1point >= 0 && (game.P2point-game.P1point) >= 2 {
-		score = "Win for player2"
-	}
 	return score
 }
 
