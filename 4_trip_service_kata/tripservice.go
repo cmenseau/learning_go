1c0b094e6fbac5726d1966222593df0f2d84ad45
cmenseau <cycy.menseau@gmail.com>
2024-07-16 16:48:46 +1200

    start refactor

--- 4_trip_service_kata/tripservice.go
+++ 4_trip_service_kata/tripservice.go
@@ -16,31 +16,24 @@ type Service struct {
 	tripDAO DaoTripFinder
 }
 
-func (service *Service) GetTripByUser(user Friender) ([]Trip, error) {
+func (service *Service) GetTripByUser(user Extrovert) ([]Trip, error) {
 	var trips []Trip
-	friends, err := user.Friends()
-	if err != nil {
-		return trips, err
-	}
+
 	loggedUser, err := loggedUserGetter.GetLoggedUser()
+
 	if err != nil {
 		return trips, err
 	}
-	var isFriend bool
-	if loggedUser != nil {
-		for _, friend := range friends {
-			if *loggedUser == friend {
-				isFriend = true
-				break
-			}
-		}
-		if isFriend {
-			return service.tripDAO.FindTripByUser(user)
-		}
-		return trips, err
-	} else {
+
+	if loggedUser == nil {
 		return trips, notLoggedUser_err
 	}
+
+	if loggedUser.IsFriendWith(user) {
+		return service.tripDAO.FindTripByUser(user)
+	}
+
+	return trips, err
 }
 
 type LoggedUserGetter interface {
@@ -54,25 +47,40 @@ func (userSession *UserSession) GetLoggedUser() (*User, error) {
 	return nil, userSession_err
 }
 
-type Friender interface {
-	Friends() ([]User, error)
+type Extrovert interface {
+	GetFriends() ([]User, error)
 }
 
 type User struct {
 }
 
-func (user *User) Friends() ([]User, error) {
+func (user *User) GetFriends() ([]User, error) {
 	var friends []User
 	return friends, nil
 }
 
+func (user *User) IsFriendWith(extrovert Extrovert) bool {
+	friends, err := extrovert.GetFriends()
+
+	if err != nil {
+		return false
+	}
+
+	for _, friend := range friends {
+		if *user == friend {
+			return true
+		}
+	}
+	return false
+}
+
 type DaoTripFinder interface {
-	FindTripByUser(user Friender) ([]Trip, error)
+	FindTripByUser(user Extrovert) ([]Trip, error)
 }
 
 type Dao struct {
 }
 
-func (dao *Dao) FindTripByUser(user Friender) ([]Trip, error) {
+func (dao *Dao) FindTripByUser(user Extrovert) ([]Trip, error) {
 	return nil, dao_err
 }
--- 4_trip_service_kata/tripservice_test.go
+++ 4_trip_service_kata/tripservice_test.go
@@ -10,7 +10,7 @@ func TestEmptyUser(test *testing.T) {
 
 	var user User
 
-	var friends, err = user.Friends()
+	var friends, err = user.GetFriends()
 
 	if friends != nil || err != nil {
 		test.Errorf("expected friends, err to be (%v, %v) got (%v, %v)", nil, nil, friends, err)
@@ -47,19 +47,19 @@ func TestUserSession(test *testing.T) {
 }
 
 type MockUser struct {
-	friends func() ([]User, error) // set it to control behavior
+	getFriends func() ([]User, error) // set it to control behavior
 }
 
 // implement Friender
-func (mu MockUser) Friends() ([]User, error) {
-	return mu.friends()
+func (mu MockUser) GetFriends() ([]User, error) {
+	return mu.getFriends()
 }
 
 func TestFriendsError(test *testing.T) {
 
 	var svc Service = Service{tripDAO: &Dao{}}
 	var mu MockUser = MockUser{
-		friends: func() ([]User, error) {
+		getFriends: func() ([]User, error) {
 			return nil, errors.New("Some error")
 		},
 	}
@@ -151,7 +151,7 @@ func TestLoggedUserError(test *testing.T) {
 func TestGetTripOfStranger(test *testing.T) {
 
 	var mu MockUser = MockUser{
-		friends: func() ([]User, error) {
+		getFriends: func() ([]User, error) {
 			return []User{User{}}, nil
 		},
 	}
@@ -178,7 +178,7 @@ type MockDao struct {
 	findTrip func() ([]Trip, error) // set it to control behavior
 }
 
-func (dao *MockDao) FindTripByUser(user Friender) ([]Trip, error) {
+func (dao *MockDao) FindTripByUser(user Extrovert) ([]Trip, error) {
 	return dao.findTrip()
 }
 
@@ -187,7 +187,7 @@ func TestGetTripOfFriend(test *testing.T) {
 	var friend User = User{}
 
 	var mu MockUser = MockUser{
-		friends: func() ([]User, error) {
+		getFriends: func() ([]User, error) {
 			return []User{User{}, friend}, nil
 		},
 	}
